6ad3a4c6bdd3ab96d65c671da00b22c3af12a0e2
Keval Navadiya <kevalnavadiya39@gmail.com>
2021-02-16 14:51:42 +0530

    get content and show is done

---
+++ popup.js
@@ -0,0 +1,17 @@
+var tab_title = "";
+function display_h1(results) {
+    h1 = results;
+    document.querySelector("#id1").innerHTML =
+        "<p>tab title: " + tab_title + "</p><p>dom h1: " + h1 + "</p>";
+}
+chrome.tabs.query({ active: true }, function (tabs) {
+    let tab = tabs[0];
+    tab_title = tab.title;
+    chrome.tabs.executeScript(
+        tab.id,
+        {
+            code: 'document.querySelector("h1").textContent',
+        },
+        display_h1
+    );
+});
