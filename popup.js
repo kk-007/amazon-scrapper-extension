dd310dc299776e3e712f24a2c0b558dc5f7c2eaf
Keval Navadiya <kevalnavadiya39@gmail.com>
2021-02-16 16:29:19 +0530

    added find element from current page

--- popup.js
+++ popup.js
@@ -1,17 +1,45 @@
-var tab_title = "";
-function display_h1(results) {
-    h1 = results;
-    document.querySelector("#id1").innerHTML =
-        "<p>tab title: " + tab_title + "</p><p>dom h1: " + h1 + "</p>";
+const _data = [
+    {
+        key: "title",
+        xpath: `\/\/*[@id=\"productTitle\"]`,
+    },
+];
+const getDocumentFromString = (domString) => {
+    const domparser = new DOMParser();
+    return domparser.parseFromString(domString, "text/html");
+};
+const getData = (document) => {
+    let data = {};
+    for (let item of _data) {
+        data[item.key] = document
+            .evaluate(
+                item.xpath,
+                document,
+                null,
+                XPathResult.FIRST_ORDERED_NODE_TYPE,
+                null
+            )
+            .singleNodeValue.innerHTML.toString()
+            .trim();
+    }
+    return data;
+};
+function displayData(data) {
+    document.querySelector("#title").innerHTML =
+        "</p><p><b>Product Name:</b> " + data[_data[0].key] + "</p>";
 }
+const displayProductDetail = (dom) => {
+    let doc = getDocumentFromString(dom);
+    let data = getData(doc);
+    displayData(data);
+};
 chrome.tabs.query({ active: true }, function (tabs) {
     let tab = tabs[0];
-    tab_title = tab.title;
     chrome.tabs.executeScript(
         tab.id,
         {
-            code: 'document.querySelector("h1").textContent',
+            code: `document.body.innerHTML`,
         },
-        display_h1
+        displayProductDetail
     );
 });
